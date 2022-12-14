e30e0ada9a1f04a490a6efc5889143a92f8b012d
McKiba Williams <44137941+Mckiba@users.noreply.github.com>
2022-12-14 09:21:00 -0500

    Update Event.route.js

--- Routes/Event.route.js
+++ Routes/Event.route.js
@@ -9,13 +9,13 @@ router.get('/', ProductController.getAllEvents);
 //Create a new event
 router.post('/', ProductController.createNewEvent);
 
-//Get a product by id
+//Get a event by id
 router.get('/:id', ProductController.findEventById);
 
-//Update a product by id
+//Update a event by id
 router.patch('/:id', ProductController.updateEvent);
 
-//Delete a product by id
+//Delete a event by id
 router.delete('/:id', ProductController.deleteAEvent);
 
 module.exports = router;
