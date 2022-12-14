ca755afda1251a994ff86a432c9d70263d0c930a
McKiba Williams <44137941+Mckiba@users.noreply.github.com>
2022-12-14 09:21:58 -0500

    Update Event.route.js

--- Routes/Event.route.js
+++ Routes/Event.route.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const ProductController = require('../Controllers/Event.Controller');
+const EventController = require('../Controllers/Event.Controller');
 
 //Get a list of all events
-router.get('/', ProductController.getAllEvents);
+router.get('/', EventController.getAllEvents);
 
 //Create a new event
-router.post('/', ProductController.createNewEvent);
+router.post('/', EventController.createNewEvent);
 
 //Get a event by id
-router.get('/:id', ProductController.findEventById);
+router.get('/:id', EventController.findEventById);
 
 //Update a event by id
-router.patch('/:id', ProductController.updateEvent);
+router.patch('/:id', EventController.updateEvent);
 
 //Delete a event by id
-router.delete('/:id', ProductController.deleteAEvent);
+router.delete('/:id', EventController.deleteAEvent);
 
 module.exports = router;
